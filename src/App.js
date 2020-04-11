979c0c61f45feafdb93b0e917fcec5eddac3c424
Winston <winvargo@gmail.com>
2020-04-11 14:15:41 -0400

    changed text in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          dungus.
         </p>
         <a
           className="App-link"
