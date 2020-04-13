8e85da610d8911a8b16637ca49b26d23c49b83c2
Winston <winvargo@gmail.com>
2020-04-13 07:36:08 -0400

    trying to get Travis to fire

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          dungus amongus
+          dungus amongus woot
         </p>
         <a
           className="App-link"
