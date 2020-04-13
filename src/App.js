c8826d5ba5e79ce6d6272bb05319f5d27f979d31
Winston <winvargo@gmail.com>
2020-04-13 08:01:09 -0400

    9th times the charm

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          dungus amongus woot
+          dungus amongus wootini
         </p>
         <a
           className="App-link"
